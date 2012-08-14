5c31aea64fea713aeb7d6df0fe3e81860a571ed8
Ha Quang Minh <minhprg@gmail.com>
2012-08-14 09:08:29 +0700

    Update algorithms

--- app.pl
+++ app.pl
@@ -2,19 +2,21 @@
 
 require "algorithms.pl";
 require "misc.pl";
+use List::MoreUtils qw/ uniq /;
 
 use utf8;
 
-@SPAM_SCORES;
-@HAM_SCORES;
+@SPAM_SCORES = ();
+@HAM_SCORES = ();
 $TESTCASE = 1;
 
 learnBayesianThreshold();
 
 
-my @arr = &getAllThreshold($TESTCASE, 1);
+my @arr = &getAllThreshold($TESTCASE, 2);
 my @b = &getDistinctThreshold(@arr);
-foreach $t (@b){
+my @c = uniq @SPAM_SCORES;
+foreach $t(@c){
 	print "$t \n";
 }
 1;
--- misc.pl
+++ misc.pl
@@ -1,7 +1,9 @@
 #!/usr/bin/perl
 sub getNK {
-	my ($set, $t) = $_;
+	my @set = $_[0];
+	my $t = $_[1];
 	my $c = 0;
+	print $_[3]."\n";
 	foreach $item (@set) {
 		if ($item == $t) {
 			$c += 1;
